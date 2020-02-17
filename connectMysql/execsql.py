864d0d1c2327c37b442d0119f08587d9956f729b
hope.gong <hope.gong@jingdigital.com>
2020-02-17 21:59:52 +0800

    fix

--- connectMysql/execsql.py
+++ connectMysql/execsql.py
@@ -1,25 +1,24 @@
 # -*- coding: UTF-8 -*-
-#yum -y install python-devel mysql-devel  &&  pip install MySQL-python
 import MySQLdb
 from connectMysql import log
 
 # execsql by sql
-def execsql(host, port, user, passwd, database, sql, result):
+def execsql(host, user, passwd, database, sql, result):
     log.log("info", "database", database)
     log.log("info", "user", user)
     log.log("info", "host", host)
     log.log("info", "sql", sql)
-    db = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, database=database)
+    db = MySQLdb.connect(host=host, port=3306, user=user, passwd=passwd, db=database)
     cursor = db.cursor()
     cursor.execute(sql)
     results = []
     if str(result) == '1':
-        results = cursor.fetchone()
+        results = cursor.fetchall()
     cursor.close()
     return results
 
 # execsql by sqlfile
-def execsqlfile(host, port, user, passwd, database, sqlfile, result):
+def execsqlfile(host, user, passwd, database, sqlfile, result):
     with open(sqlfile, 'r') as f:
         sql = f.read()
     results = execsql(host, port, user, passwd, database, sql, result)
