6421216b21e2535ad5f3e33899b2310e0d0a5085
tomatohope <45392995+tomatohope@users.noreply.github.com>
2020-02-21 19:54:06 +0800

    Update azkaban-project.py

--- connectPgsql/azkaban-project.py
+++ connectPgsql/azkaban-project.py
@@ -5,34 +5,40 @@ import os
 # getSessinId = str("curl -k -X POST --data \"action=login&username=azkaban&password=azkaban\" http://192.168.43.63:8081/")
 # print("getSessinIdCmd:", getSessinId)
 # os.system(getSessinId)
-sessionId = "510532ed-0df1-4a2f-a475-0c1547e788b8"
+sessionId = "32869fd5-b3b9-485b-96db-b026643b6ffe"
 
-for i in range(31, 61):
+for i in range(1, 101):
     print("i: ", i)
-
+    #
     # # createProject
-    # createProject = str("curl -k -X POST --data \"session.id="）+ str(sessionId) + str("&name=aaaa") + str(i) + str("&description=test\" http://192.168.43.63:8081/manager?action=create")
-    # print("createProjectCmd: ", createProject)
+    # createProject = str("curl -k -X POST --data \"session.id=") + str(sessionId) + str("&name=aaaa") + str(i) + str("&description=test\" http://10.80.0.231:8081/manager?action=create")
+    # print("createProjectCmd:", createProject)
     # os.system(createProject)
     #
     # # uploadfile
-    # uploadfile = str("curl -k -i -X POST --form 'session.id=") + str(sessionId) + str("' --form 'ajax=upload' --form 'file=@flow1.zip;type=application/zip' --form 'project=aaaa") + str(i) + str("' http://192.168.43.63:8081/manager")
+    # uploadfile = str("curl -k -i -X POST --form 'session.id=") + str(sessionId) + str("' --form 'ajax=upload' --form 'file=@test-flow-mysql.zip;type=application/zip' --form 'project=aaaa") + str(i) + str("' http://10.80.0.231:8081/manager")
     # print("uploadfileCmd: ", uploadfile)
     # os.system(uploadfile)
 
     #deleteProject
-    # deleteProject = str("curl -k --get --data \"session.id=") + str(sessionId) + str("&delete=true&project=aaaa") + str(i) + str("\" http://192.168.43.63:8081/manager")
+    # deleteProject = str("curl -k --get --data \"session.id=") + str(sessionId) + str("&delete=true&project=aaaa") + str(i) + str("\" http://10.80.0.231:8081/manager")
     # print("deleteProjectCmd: ", deleteProject)
     # os.system(deleteProject)
 
-    #schedule parameter projects
-    projectId = int(i)
-    schedule = str("curl -k http://192.168.43.63:8081/schedule -d \"ajax=scheduleFlow&is_recurring=on&period=1m&flowOverride[database]=dev_jing_dw&flowOverride[user]=postgres&flowOverride[password]=postgres&flowOverride[host]=192.168.43.63&flowOverride[port]=5432&flowOverride[sqlfile]=/tmp/3.sql&projectName=aaaa") + str(i) + str("&flow=flow1&projectId=") + str(projectId) + str("&scheduleTime=16,59,pm,PDT&scheduleDate=02/12/2020\" -b azkaban.browser.session.id=") + str(sessionId)
-    print("scheduleCmd:", schedule)
-    os.system(schedule)
+    #schedule parameter projects Pgsql
+    # projectId = int(i) + 2
+    # schedule = str("curl -k http://10.80.0.231:8081/schedule -d \"ajax=scheduleFlow&is_recurring=on&period=1h&flowOverride[database]=dev_jms_dw&flowOverride[user]=dev_jms_dw_owner&flowOverride[password]=C#UzrAyBstcV6Dpp&flowOverride[host]=gp-uf6fw1e6xv35ikrj9.gpdb.rds.aliyuncs.com&flowOverride[port]=3432&flowOverride[sqlfile]=/opt/services/azkaban/azkaban-exec-server/build/install/azkaban-exec-server/bin/sql/test-sql.sql&projectName=aaaa") + str(i) + str("&flow=test-flow&projectId=") + str(projectId) + str("&scheduleTime=02,42,am,PDT&scheduleDate=02/21/2020\" -b azkaban.browser.session.id=") + str(sessionId)
+    # print("scheduleCmd:", schedule)
+    # os.system(schedule)
+
+    #schedule parameter projects Mysql
+    # projectId = int(i) + 104
+    # schedule = str("curl -k http://10.80.0.231:8081/schedule -d \"ajax=scheduleFlow&is_recurring=on&period=1h&flowOverride[database]=dev_dw&flowOverride[user]=dev_ads_mysql&flowOverride[password]=gAwEb178XHc3Ir8L&flowOverride[host]=am-uf68cpts08jf19trn131910.ads.aliyuncs.com&flowOverride[sqlfile]=/opt/services/azkaban/azkaban-exec-server/build/install/azkaban-exec-server/bin/sql/Mysql-test-sql.sql&projectName=aaaa") + str(i) + str("&flow=test-flow&projectId=") + str(projectId) + str("&scheduleTime=19,34,pm,PDT&scheduleDate=02/21/2020\" -b azkaban.browser.session.id=") + str(sessionId)
+    # print("scheduleCmd:", schedule)
+    # os.system(schedule)
 
     #unschedule parameter projects
-    # scheduleId = int(i) + 520
-    # unschedule = str("curl -k http://192.168.43.63:8081/schedule -d \"action=removeSched&scheduleId=") + str(scheduleId) + str("\" -b azkaban.browser.session.id=") + str(sessionId)
-    # print("unscheduleCmd:", unschedule)
-    # os.system(unschedule)
+    scheduleId = int(i) + 464
+    unschedule = str("curl -k http://10.80.0.231:8081/schedule -d \"action=removeSched&scheduleId=") + str(scheduleId) + str("\" -b azkaban.browser.session.id=") + str(sessionId)
+    print("unscheduleCmd:", unschedule)
+    os.system(unschedule)
